32755649f8eabeae4a7ec7b9e130e6d87fb31644
taylorbaker <tdbaker2@asu.edu>
2021-12-31 14:35:59 -0700

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+function findTheOldest(input) {
+  const ages = input.map(person => {
+    let today = new Date();
+    let currentYear = today.getFullYear(); // need to create Date() obj before calling getFullYear() method
+    let term = person.yearOfDeath;
 
+    // if living, use current year for age calc
+    if (!person.hasOwnProperty('yearOfDeath')) { // if person has not died
+      term = currentYear;
+    }
+  
+    return term - person.yearOfBirth; // return age
+  });
+
+  const maxValue = Math.max(...ages);
+  const maxIndex = ages.indexOf(maxValue);
+	
+  return input[maxIndex];
 };
 
 // Do not edit below this line
